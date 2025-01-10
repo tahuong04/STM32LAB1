e56e5f0897bd25987b4d1bc2446dddc6cbccf9bb
LAPTOP-VI5HCIOH\PC-Huong <tahuong2004vp@gmail.com>
2025-01-10 19:40:20 +0700

    Init for test2

--- source_code/Core/Src/main.c
+++ source_code/Core/Src/main.c
@@ -65,6 +65,7 @@ int main(void)
 
   /* USER CODE BEGIN 1 */
 	//TODO test1
+	//TODO test2
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
